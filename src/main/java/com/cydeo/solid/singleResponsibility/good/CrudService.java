bd5fb1f8307ac530d3b6ad53d27b63326823382c
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-23 23:51:37 +0300

    CrudService.java

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/good/CrudService.java
@@ -0,0 +1,22 @@
+package com.cydeo.solid.singleResponsibility.good;
+
+public class CrudService {    public static void create() {
+    // Create a user
+    // Send user to database related part
+}
+
+    public static void read() {
+        // Read a user
+        // Send user to database related part
+    }
+
+    public static void update() {
+        // Update a user
+        // Send user to database related part
+    }
+
+    public static void delete() {
+        // Delete a user
+        // Send user to database related part
+    }
+}
