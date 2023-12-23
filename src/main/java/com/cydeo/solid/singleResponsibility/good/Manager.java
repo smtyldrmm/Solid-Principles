183c511edb2fff066331f7ac079e2244300976e2
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-23 23:52:16 +0300

    Manager.java

---
+++ src/main/java/com/cydeo/solid/singleResponsibility/good/Manager.java
@@ -0,0 +1,12 @@
+package com.cydeo.solid.singleResponsibility.good;
+
+public class Manager {    public static boolean validate() {
+    // Check validation
+    return true;
+}
+
+    public static boolean checkSecurity() {
+        // Send to security related part
+        return true;
+    }
+}
