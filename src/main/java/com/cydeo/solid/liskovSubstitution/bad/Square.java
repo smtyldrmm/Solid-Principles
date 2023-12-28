c313b38005566db942bc1930444031aaf7d10013
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-28 09:51:45 +0300

    LiskovSubstitutionBad

---
+++ src/main/java/com/cydeo/solid/liskovSubstitution/bad/Square.java
@@ -0,0 +1,16 @@
+package com.cydeo.solid.liskovSubstitution.bad;
+
+public class Square extends Rectangle {
+
+    @Override
+    public void setHeight(int height) {
+        this.height = height;
+        this.width = height;
+    }
+
+    @Override
+    public void setWidth(int width) {
+        this.width = width;
+        this.height = width;
+    }
+}
