49471b2d3a37e4eb4decbdb085e5b239603dc9ab
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-28 12:04:12 +0300

    InterfaceSegregationBad

---
+++ src/main/java/com/cydeo/solid/interfaceSegregation/bad/Bird.java
@@ -0,0 +1,8 @@
+package com.cydeo.solid.interfaceSegregation.bad;
+
+public interface Bird {
+    void eat();
+    void walk();
+    void fly();
+
+}
---
+++ src/main/java/com/cydeo/solid/interfaceSegregation/good/Chicken.java
@@ -0,0 +1,14 @@
+package com.cydeo.solid.interfaceSegregation.good;
+
+public class Chicken implements CanEat,CanWalk{
+
+    @Override
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Walking");
+    }
+}
