c9c8680047a1e74d1fbcd4e44eeb5efb8e2457e6
yildirimsamet <yildirimsamet1@gmail.com>
2023-12-29 22:14:04 +0300

    OpenClosed

---
+++ src/main/java/com/cydeo/solid/openClosed/example2/good/Calculator.java
@@ -0,0 +1,13 @@
+package com.cydeo.solid.openClosed.example2.good;
+
+public class Calculator {
+
+    public void calculate(Operation operation) {
+
+        int num1 = 10;
+        int num2 = 2;
+
+        operation.calculate(num1, num2);
+
+    }
+}
